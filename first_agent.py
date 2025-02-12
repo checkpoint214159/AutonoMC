895bcc5d9d3148723811fd7c5df9cead027a1fa0
ben <bengoh04@gmail.com>
2025-02-13 00:35:36 +0800

    Moved to MineDojo, troubleshoot some of their install issues (yet to make Docker image)

--- first_agent.py
+++ first_agent.py
@@ -1,25 +1,14 @@
-import gym
-import minerl
-import logging
-import time
+import minedojo
 
-logging.basicConfig(level=logging.DEBUG)
-env = gym.make('MineRLBasaltFindCave-v0')
-
-
-# Note that this command will launch the MineRL environment, which takes time.
-# Be patient!
+env = minedojo.make(
+    task_id="harvest_wool_with_shears_and_sheep",
+    image_size=(160, 256)
+)
 obs = env.reset()
-
-done = False
-
-while not done:
-    time.sleep(1)
-    # Take a random action
-    action = env.action_space.sample()
-    # In BASALT environments, sending ESC action will end the episode
-    # Lets not do that
-    action["ESC"] = 0
-    logging.info(action)
-    obs, reward, done, _ = env.step(action)
-    env.render()
+for i in range(50):
+    act = env.action_space.no_op()
+    act[0] = 1    # forward/backward
+    if i % 10 == 0:
+        act[2] = 1    # jump
+    obs, reward, done, info = env.step(act)
+env.close()
