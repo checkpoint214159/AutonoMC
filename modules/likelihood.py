a737300a33f554c61c0446ab610276da972168fa
root <root@DESKTOP-G7EG8KU>
2025-03-29 15:25:17 +0800

    laid out instructions and plans for myself, very basic start on some modules

---
+++ modules/likelihood.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+
+
+class Likelihood(nn.Module):
+    """
+    Module to 
+    """
---
+++ modules/transitions.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+
+
+class TransitionsModel(nn.Module):
+    """
+    Model that predicts state transitions given some policy conditioning.
+    """
+    def __init__(self):
+
+    def pred(self, policy):
+        
